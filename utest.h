669990a7514fa9c1f4e3e1cd3f1fad259c1e37a2
aracyla-note <almeidaleonardor@gmail.com>
2019-05-07 18:55:30 +0200

    fix on printing functions names

--- utest.h
+++ utest.h
@@ -18,10 +18,10 @@ int tests_ok = 0;
 #define __fail() fprintf(stderr, "ERROR: %s in line %d\n", __func__, __LINE__)
 
 //@Description: assert an expression
-#define __assert(t) do { if(!(t)) {__fail(); return 1; } else { fprintf(stdout, "OK: %s\n", __func__);}} while(0)
+#define __assert(t) do { if(!(t)) {__fail(); return 1; }} while(0)
 
 //@Description: macro used to run the tests
-#define __run_test(t) do { int r = t(); tests_run++; if(r == 0) tests_ok++;} while(0)
+#define __run_test(t) do { int r = t(); tests_run++; if(r == 0) {tests_ok++; fprintf(stdout, "OK: %s\n", #t);}} while(0)
 
 //@Description: macro to print the results of the test
 #define __print_result() do { fprintf(stdout, "\n=======Summary======\nTests: %d\nPassed: %d\n", tests_run, tests_ok);} while(0)
